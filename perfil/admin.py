cc104c18ad36622f00745cd7a2ba624fb498f31b
guidoiri <guidoirigoyen@gmail.com>
2022-03-19 11:59:58 -0300

    estructura de los models a usar

--- perfil/admin.py
+++ perfil/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Movies, Series, Songs
+
+admin.site.register(Movies)
+admin.site.register(Songs)
+admin.site.register(Series)
+
 
 # Register your models here.
--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Movies(models.Model):
+    title = models.CharField(max_length=35)
+    director = models.CharField(max_length=30)
+    length_minutes = models.IntegerField()
+    gender = models.CharField(max_length=20)
+    
+    
+class Series(models.Model):
+    title = models.CharField(max_length=35)
+    director = models.CharField(max_length=30)
+    length_minutes = models.IntegerField()
+    gender = models.CharField(max_length=20)
+    seasons = models.IntegerField()
+    
+    
+class Songs(models.Model):
+    title = models.CharField(max_length=35)
+    artist = models.CharField(max_length=30)
+    length_minutes = models.IntegerField()
+    gender = models.CharField(max_length=20)
+
+ 
